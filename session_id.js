70afa38c1f5f436d5f29921b9dc774134d50a568
Hab546 <sa7419404@gmail.com>
2024-12-17 04:14:20 +0500

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -21,14 +21,14 @@
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
 //  * @project_name : MANU-MD
-//  * @author : Manul Official
+//  * @author : Mr devil 
 //  * @youtube : https://www.youtube.com/@ManulOfficialTech
 //  * @description : Manu-MD ,A Multi-functional whatsapp user bot.
 //*
 //*
 //base by Manul Official
 //GitHub: @manulofficial7
-//WhatsApp: +94742274855
+//WhatsApp: +923187379684
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@ManulOfficialTech
 //   * Created By Github: manulofficial7.
 //   * Credit To Manul Official
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&uQ0n3AwI#s3futA-9zR-YHG3qQc-X3RWk4og08qWaIr8d9t4_vBM",
 };
